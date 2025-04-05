b59566b3db8ba56f887a5eeba7d3c143cabad238
Taseny <2218958215@qq.com>
2025-04-05 15:54:12 +0800

    修复使用malloc函数分配内存时导致f_mkfs()报错

--- FATFS/App/fatfs.c
+++ FATFS/App/fatfs.c
@@ -42,7 +42,7 @@ void fatfs_init(void)
       ips160_show_string(0, 0, "FR_NO_Filesystem Error\nmake filesystem...", RGB565_WHITE, RGB565_RED);
 #endif
       BYTE *work = malloc(sizeof(BYTE) * _MAX_SS);
-      retUSER = f_mkfs("FL:", FM_FAT, _MAX_SS, work, sizeof(work));
+      retUSER = f_mkfs("FLASH:", FM_FAT, _MAX_SS, work, _MAX_SS);
       free(work);
 #if USE_IPS_ASSERT == 1
       if (retUSER == FR_OK) ips160_show_string(0, 24, "Make filesystem done\ntrying to remount SPI Flash...", RGB565_WHITE, RGB565_RED);
