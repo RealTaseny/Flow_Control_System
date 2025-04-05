b1858ca697b9357b010a1d28771ebe4ece9198ce
Taseny <2218958215@qq.com>
2025-04-06 00:19:08 +0800

    添加流量计

--- Hardware_Drivers/src/flow_counter.c
+++ Hardware_Drivers/src/flow_counter.c
@@ -3,3 +3,60 @@
 //
 
 #include "flow_counter.h"
+
+uint32_t flow_counter = 0, flow_counter_last = 0;
+
+void flow_counter_init(void)
+{
+    /* USER CODE BEGIN TIM3_Init 0 */
+
+    /* USER CODE END TIM3_Init 0 */
+
+    TIM_SlaveConfigTypeDef sSlaveConfig = {0};
+    TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+    /* USER CODE BEGIN TIM3_Init 1 */
+
+    /* USER CODE END TIM3_Init 1 */
+    htim3.Instance = TIM3;
+    htim3.Init.Prescaler = 0;
+    htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim3.Init.Period = 65535;
+    htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
+    sSlaveConfig.InputTrigger = TIM_TS_TI1FP1;
+    sSlaveConfig.TriggerPolarity = TIM_TRIGGERPOLARITY_RISING;
+    sSlaveConfig.TriggerFilter = 0;
+    if (HAL_TIM_SlaveConfigSynchro(&htim3, &sSlaveConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN TIM3_Init 2 */
+
+    /* USER CODE END TIM3_Init 2 */
+	HAL_TIM_Base_Start_IT(&htim3);
+}
+
+static void get_flow_counter(void)
+{
+	flow_counter = flow_counter + (__HAL_TIM_GetCounter(&htim3));
+	__HAL_TIM_SetCounter(&htim3, 0);
+}
+
+void get_flow_speed(float *flow_speed, uint8_t get_times_ms)
+{
+    flow_counter_last = flow_counter;
+    get_flow_counter();
+    *flow_speed = (float)(flow_counter - flow_counter_last) / ((float)get_times_ms * FLOW_COUNT_PER_L);
+}
